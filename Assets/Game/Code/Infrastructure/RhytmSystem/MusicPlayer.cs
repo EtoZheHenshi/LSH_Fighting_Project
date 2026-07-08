1693b7309c4020cddf6897518d8157b8aa895c88
Ilya Pchelkin <ilyaipka@gmail.com>
2026-07-08 21:36:33 +0300

    created RhytmSystem
    added metronome sound + visualizer

---
+++ Assets/Game/Code/Infrastructure/RhytmSystem/MusicPlayer.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace Code.Infrastructure.RhytmSystem
+{
+    /// <summary>
+    /// Управляет воспроизведением музыкального трека (загрузка, запуск, пауза, остановка).
+    /// Отвечает за синхронизацию текущего времени воспроизведения с Store.
+    /// В будущем будет содержать ссылку на AudioSource и обновлять позицию в Store 
+    /// через вызов Store.UpdateMusicPosition(float)
+    /// </summary>
+    public class MusicPlayer : Singleton<MusicPlayer>
+    {
+        public AudioSource music;
+        public bool startPlaying;
+        public BeatTracker beatTracker;
+        private float _timePositionMs;
+
+        private void Start()
+        {
+            Store.Instance.UpdateMusicPosition(music.time); 
+        }
+
+        private void Update()
+        {
+            Store.Instance.UpdateMusicPosition(music.time);
+        }
+    }
+}
